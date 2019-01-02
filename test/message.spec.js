b535bf5efa25130281a6c31baae9182bae489398
Yaroslav Savaryn <yariksav@gmail.com>
2019-01-02 18:56:54 +0200

    Add test for message, fix notify test

--- test/message.spec.js
+++ test/message.spec.js
@@ -13,39 +13,39 @@ describe('manager', () => {
   })
 
   test('Test success notification', async () => {
-    const dlg = await manager.notify.success('Test notification', {
+    const dlg = await manager.message.success('Test message', {
       timeout: 100,
       waitForResult: false
     })
     await sleep(50)
     expect(dlg.vm.$el).toMatchSnapshot()
-    await sleep(100)
+    await sleep(600)
     await Vue.nextTick()
     expect(document.body.innerHTML).toBe('')
   })
 
   test('Test error notification with top-left position', async () => {
-    const dlg = await manager.notify.error('Test notification', {
+    const dlg = await manager.message.error('Test message', {
       timeout: 100,
       position: 'top-left',
       waitForResult: false
     })
     await sleep(50)
     expect(dlg.vm.$el).toMatchSnapshot()
-    await sleep(500)
+    await sleep(600)
     await Vue.nextTick()
     expect(document.body.innerHTML).toBe('')
   })
 
   test('Test warning notification with bottom-right position', async () => {
-    const dlg = await manager.notify.warning('Test notification', {
+    const dlg = await manager.message.warning('Test message', {
       timeout: 100,
       position: 'bottom-right',
       waitForResult: false
     })
     await sleep(50)
     expect(dlg.vm.$el).toMatchSnapshot()
-    await sleep(1000)
+    await sleep(600)
     await Vue.nextTick()
     expect(document.body.innerHTML).toBe('')
   })
