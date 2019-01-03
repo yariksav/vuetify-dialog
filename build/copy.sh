48a80813f8381ad9119493a68040deb3c226ddd7
Yaroslav Savaryn <yariksav@gmail.com>
2019-01-04 01:02:50 +0200

    (feat): build tools

---
+++ build/copy.sh
@@ -0,0 +1,11 @@
+
+shopt -s extglob  # to enable extglob
+PROJECT_NAME=$(node -p "require('./package.json').name")
+pid=$(cat ./build/related)
+PROJECTS=($(echo $pid | tr " " "\n"))
+for project in "${PROJECTS[@]}"
+do
+  COPY_PATH=$project/node_modules/$PROJECT_NAME/
+  cp -r !(node_modules*|build*|coverage*|test*) $COPY_PATH
+  echo "copied to $COPY_PATH"
+done
