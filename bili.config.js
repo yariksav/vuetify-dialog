a5c3eff6a210596f6050532b8069913ec37f1fba
Yaroslav Savaryn <yariksav@gmail.com>
2019-07-11 00:16:31 +0300

    update bili and config

--- bili.config.js
+++ bili.config.js
@@ -1,16 +1,18 @@
 module.exports = {
   name: 'vuetify-dialog',
-  input: './src/index.js',
-  format: ['es', 'cjs', 'umd', 'umd-min'],
-  plugins: ['vue'],
-  // vue: {
-  //   someOption: "value'
-  // },
+  input: 'src/index.js',
+  output: {
+    dir: './dist',
+    name: 'vuetify-dialog',
+    moduleName: 'dfdfdfd',
+    extractCSS: false,
+    format: ['es', 'cjs', 'umd', 'umd-min']
+  },
+  plugins: {
+    vue: true
+  },
   globals: {
     vue: 'Vue'
   },
-  external: ['vue'],
-  postcss: {
-    extract: false
-  }
+  external: ['vue']
 }
